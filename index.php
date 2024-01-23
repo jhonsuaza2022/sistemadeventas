9ce28bb96ad505af68586d05c47c92083d2c3f3c
Jhon Suaza <jhonalexandersuazabedoya@gmail.com>
2024-01-23 00:46:49 -0500

    VENTAS

--- index.php
+++ index.php
@@ -8,6 +8,8 @@ include ('app/controllers/roles/listado_de_roles.php');
 include ('app/controllers/categorias/listado_de_categoria.php');
 include ('app/controllers/almacen/listado_de_productos.php');
 include ('app/controllers/proveedores/listado_de_proveedores.php');
+include ('app/controllers/compras/listado_de_compras.php');
+include ('app/controllers/ventas/listado_de_ventas.php');
 ?>
 
 <!-- Content Wrapper. Contains page content -->
@@ -157,6 +159,57 @@ include ('app/controllers/proveedores/listado_de_proveedores.php');
                     </div>
                 </div>
 
+
+
+
+
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-danger">
+                        <div class="inner">
+                            <?php
+                            $contador_de_compras = 0;
+                            foreach ($compras_datos as $compras_dato){
+                                $contador_de_compras = $contador_de_compras + 1;
+                            }
+                            ?>
+                            <h3><?php echo $contador_de_compras;?></h3>
+                            <p>Compras Registrados</p>
+                        </div>
+                        <a href="<?php echo $URL;?>/compras">
+                            <div class="icon">
+                                <i class="fas fa-cart-plus"></i>
+                            </div>
+                        </a>
+                        <a href="<?php echo $URL;?>/compras" class="small-box-footer">
+                            Más detalle <i class="fas fa-arrow-circle-right"></i>
+                        </a>
+                    </div>
+                </div>
+
+
+
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-warning">
+                        <div class="inner">
+                            <?php
+                            $contador_de_ventas = 0;
+                            foreach ($ventas_datos as $ventas_dato){
+                                $contador_de_ventas = $contador_de_ventas + 1;
+                            }
+                            ?>
+                            <h3><?php echo $contador_de_ventas;?></h3>
+                            <p>Ventas Registradas</p>
+                        </div>
+                        <a href="<?php echo $URL;?>/ventas">
+                            <div class="icon">
+                                <i class="fas fa-shopping-basket"></i>
+                            </div>
+                        </a>
+                        <a href="<?php echo $URL;?>/ventas" class="small-box-footer">
+                            Más detalle <i class="fas fa-arrow-circle-right"></i>
+                        </a>
+                    </div>
+                </div>
 
 
             </div>
--- layout/parte1.php
+++ layout/parte1.php
@@ -222,6 +222,33 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
                     <li class="nav-item ">
                         <a href="#" class="nav-link active">
+                            <i class="nav-icon fas fa-shopping-basket"></i>
+                            <p>
+                                Ventas
+                                <i class="right fas fa-angle-left"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="<?php echo $URL;?>/ventas" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Listado de Ventas</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="<?php echo $URL;?>/ventas/create.php" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Realizar Venta</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+
+
+
+
+                    <li class="nav-item ">
+                        <a href="#" class="nav-link active">
                             <i class="nav-icon fas fa-car"></i>
                             <p>
                                 Proveedores
